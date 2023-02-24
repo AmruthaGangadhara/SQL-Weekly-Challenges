15076a53c322dba3cdde724d205d8f0952530534
Amrutha Gangadhara <amruthanaidu97@gmail.com>
2023-02-23 16:16:04 -0800

    Add files via upload

---
+++ Week_2/week_2.sql
@@ -0,0 +1,14 @@
+Question 1
+Find the result of subtracting the timestamp '2012-07-30 01:00:00' from the timestamp '2012-08-31 01:00:00'
+
+Solution:
+SELECT timestamp '2012-08-31 01:00:00'- timestamp '2012-07-30 01:00:00' as  interval
+
+
+Question 2
+Return a count of bookings for each month, sorted by month
+
+SELECT date_trunc('month',starttime),count(*) --selecting the month part of the timestamp
+FROM cd.bookings
+GROUP BY date_trunc('month',starttime)        --Grouping by the month and sorting the result by date
+ORDER BY 1 

ea87efe9a3f72229689e4e0d1727be1b8e2396ec
Amrutha Gangadhara <amruthanaidu97@gmail.com>
2023-03-07 20:06:07 -0800

    Add files via upload

--- Week_6/week_6.sql
+++ Week_6/week_6.sql
@@ -8,15 +8,33 @@ select memid,regexp_replace(telephone,'\D','','g') from cd.members --replaced an
 
 Solution 2: 
 with t1 as (select memid, 
-regexp_match(telephone, '\D*([0-9]{3})\D*([0-9]{3})\D*([0-9]{4})') regex     --used regex to match the digits which will result in a 
+regexp_match(telephone, '[\(|\)|\-|\s]([0-9]{3})[\(|\)|\-|\s][\(|\)|\-|\s]([0-9]{3})[\(|\)|\-|\s]([0-9]{4})') regex     --used regex to match the digits which will result in a 
 from cd.members)                                                                                                        --set of 3 matches. 
 select t1.memid,concat(t1.regex[1],t1.regex[2],t1.regex[3]) telephone                                                    --concatenetaed the 3 matches of sets as telephone
 from t1
 
 /*Question 2
 
-Output the names of all members, formatted as 'Surname, Firstname'
-
+Produce a list of the total number of slots booked per facility per month in the year of 2012.
+ In this version, include output rows containing totals for all months per facility, and a total for all months for all facilities. 
+The output table should consist of facility id, month and slots, sorted by the id and month. 
+When calculating the aggregated values for all months and all facids, return null values in the month and facid columns..*/
+ 
 Solution: 
-SELECT concat(surname,',',' ',firstname) as name
-FROM cd.members
+
+WITH T1 AS (SELECT f.facid facid,date_part('month',b.starttime) as month,sum(b.slots) as slots
+FROM cd.facilities f INNER JOIN cd.bookings b
+on f.facid=b.facid
+WHERE date_part('year',b.starttime)='2012'
+GROUP BY 1,2
+ORDER BY 1,2)
+SELECT facid,month,slots
+FROM T1
+UNION ALL
+SELECT facid,NULL,sum(slots) slots
+FROM T1
+GROUP BY 1
+UNION ALL
+SELECT null,null,sum(slots) slots
+FROM T1
+ORDER BY facid,month
